8b5a29464b9ed41e80fc0b88024fa1126128d056
gulshan07dev <gulshanku9135@gmail.com>
2023-12-04 19:56:18 +0530

    setup client environment

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-center bg-red-500">
+      <h1 className="text-white text-[50px]">Youtube Like App</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ['Nunito', 'sans-serif'],
+        nunito_sans: ['"Nunito Sans"', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif'],
+        hedvig_letters: ['"Hedvig Letters Serif"', 'serif'],
+      }
+    },
+  },
+  plugins: [],
+  darkMode: ["class"]
+}
