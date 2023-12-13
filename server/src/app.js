79380e16fde88b7f56b71edae0c28b07851bae22
gulshan07dev <gulshanku9135@gmail.com>
2023-12-13 10:32:17 +0530

    start the server

--- server/src/app.js
+++ server/src/app.js
@@ -1,19 +1,9 @@
+import express from "express"
 
-/**
- * Tomorow - Todo
- */
+const app = express();
 
-// wake up: 7 max
-// almost 1 hours walking
-// 9 - 10: breakfast
+app.get("/", (req, res) => {
+    res.send("hello")
+})
 
-// 1. building MERNtube: 10 - 12
-// 2. building socialnex: 12 - 2
-// 3. Youtube Tutorial: 2 - 3
-// 4. self improvement: 3 - 6, like sm, english, podcast, etc
-
-// 6 - 7: dinner + walking
-
-// 7 - 8: working on my skills
-// 8 - 9: some code + todo plan
-// 9: good night
+export default app; 
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,12 @@
 import { config } from "dotenv"
 import connectDB from "./db/index.js"
-config({ path: "./.env" })
+import app from "./app.js";
 
+config({ path: "./.env" }) 
+const PORT = process.env.PORT || 800;
 
-connectDB();
+connectDB(); 
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+});
