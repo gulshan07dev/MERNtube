601684d065c57ac3bc6f2c14689c1a0e004b92ac
gulshan07dev <gulshanku9135@gmail.com>
2023-12-20 17:57:29 +0530

    fix a minor bug in cloudinary utils

--- server/src/utils/cloudinary.js
+++ server/src/utils/cloudinary.js
@@ -1,5 +1,4 @@
 import { v2 as cloudinary } from 'cloudinary';
-import fs from "fs";
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -16,9 +15,7 @@ export const uploadOnCloudinary = async (localFilePath) => {
         })
         return { key: response?.public_id, url: response?.url }
     } catch (error) {
-        return null
-    } finally {
-        fs.unlinkSync(localFilePath);
+        throw error
     }
 }
 
@@ -27,6 +24,6 @@ export const deleteOnCloudinary = async (public_id) => {
         if (!public_id) return null;
         return await cloudinary.uploader.destroy(public_id);
     } catch (error) {
-        return null
+        throw error
     }
 }
