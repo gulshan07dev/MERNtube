cb726f3789e9f44aade774da5c197e79e7f19730
gulshan07dev <gulshanku9135@gmail.com>
2024-01-09 12:13:51 +0530

    add subscription controller and routes

---
+++ server/src/routes/subscription.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { verifyJwt } from "../middlewares/auth.middleware.js"
+import {
+    toggleSubscription,
+    getUserChannelSubscribers,
+    getSubscribedChannels
+} from "../controllers/subscription.controller.js";
+
+const router = Router();
+router.use(verifyJwt)
+
+router.route("/c/:channelId")
+    .post(toggleSubscription)
+    .get(getUserChannelSubscribers)
+
+router.route("/u/:subscriberId")
+    .get(getSubscribedChannels)
+
+export default router;
