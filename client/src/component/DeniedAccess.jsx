4018d12b8d969aadcb80f8e8d48b37a30a64d23a
gulshan07dev <gulshanku9135@gmail.com>
2024-01-10 20:49:39 +0530

    add denied access component

---
+++ client/src/component/DeniedAccess.jsx
@@ -0,0 +1,38 @@
+import { FaExclamationCircle, FaSignInAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+import Layout from "@/layout/Layout";
+
+export default function DeniedAccess() {
+  return (
+    <Layout className="min-h-screen flex justify-center items-center">
+      <div className="w-[500px] max-w-[92%] flex flex-col gap-8 md:py-14 md:px-8 px-4 py-16 bg-white border-[1px] border-slate-300 shadow-sm rounded-xl">
+        <div className="flex items-center justify-center gap-2 text-red-500">
+          <FaExclamationCircle className="text-3xl" />
+          <h1 className="text-2xl font-bold font-hedvig_letters">Access Denied</h1>
+        </div>
+        <p className="text-gray-700 text-base font-roboto">
+          Oops! It seems like you are not authenticated and don't have the
+          necessary permissions to access this page. Please login or signup to
+          access the page.
+        </p>
+        <div className="flex items-center justify-center gap-5 font-nunito_sans">
+          <Link
+            to="/auth/login"
+            className="flex items-center gap-3 px-4 py-2 text-white bg-blue-500 rounded-md transition duration-300 hover:bg-blue-600"
+          >
+            <FaSignInAlt />
+            Login
+          </Link>
+          <Link
+            to="/auth/signup"
+            className="flex items-center gap-3 px-4 py-2 text-white bg-green-500 rounded-md transition duration-300 hover:bg-green-600"
+          >
+            <FaSignInAlt />
+            Sign Up
+          </Link>
+        </div>
+      </div>
+    </Layout>
+  );
+}
--- client/src/helper/auth/AuthRequired.jsx
+++ client/src/helper/auth/AuthRequired.jsx
@@ -1,8 +1,9 @@
+import DeniedAccess from "@/component/DeniedAccess";
 import { useSelector } from "react-redux";
-import { Outlet, Navigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 export default function AuthRequired() {
   const { isLoggedIn } = useSelector((state) => state.auth);
 
-  return isLoggedIn ? <Outlet /> : <Navigate to="/auth/login" />;
+  return isLoggedIn ? <Outlet /> : <DeniedAccess />;
 }
