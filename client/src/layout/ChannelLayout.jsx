f3bdddea5b9d6fd9c9cbfad15630a7a93c6f5987
gulshan07dev <gulshanku9135@gmail.com>
2024-01-11 15:47:10 +0530

    fix channel layout

--- client/src/layout/ChannelLayout.jsx
+++ client/src/layout/ChannelLayout.jsx
@@ -7,7 +7,6 @@ import Layout from "./Layout";
 import { getChannel } from "@/store/slices/authSlice";
 import useApiHandler from "@/hooks/useApiHandler";
 import Avatar from "@/component/Avatar";
-import Button from "@/component/Button";
 import Skeleton from "@/component/skeleton/Skeleton";
 
 export default function ChannelLayout() {
@@ -44,105 +43,117 @@ export default function ChannelLayout() {
     fetchChannel();
   }, []);
 
+  const channelTabsLink = [
+    {
+      label: "Home",
+      slug: `/c/${username}`,
+    },
+    {
+      label: "Videos",
+      slug: `/c/${username}/videos`,
+    },
+    {
+      label: "Tweets",
+      slug: `/c/${username}/tweets`,
+    },
+    {
+      label: "Playlists",
+      slug: `/c/${username}/playlists`,
+    },
+  ];
+
+  const ChannelLayoutSkeleton = () => (
+    <div className="w-full flex flex-col gap-8">
+      <Skeleton className="w-full md:h-44 h-28 rounded-2xl" />
+      <div className="w-full flex md:gap-14 gap-5">
+        <Skeleton className="md:h-36 h-20 md:w-36 w-20 rounded-full" />
+        <div className="flex flex-col md:gap-4 gap-2">
+          <Skeleton className="max-w-[200px] w-64 h-6" />
+          <Skeleton className="max-w-[150px] w-44 h-4" />
+          <Skeleton className="max-w-[150px] w-44 h-4" />
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     <Layout className="md:px-4 px-2 flex flex-col gap-6">
       {error ? (
+        // Display error message if there's an error
         <div>
           <p>{error}</p>
         </div>
       ) : (
+        // Render channel details if no error
         <div className="w-full flex flex-col gap-8">
-          <div className="w-full md:h-44 h-28 rounded-2xl overflow-hidden">
-            {loading ? (
-              <Skeleton className="h-full w-full rounded-2xl" />
-            ) : (
-              <img
-                src={channel?.coverImage?.url || "/default-cover.png"}
-                className="w-full h-full object-cover"
-                alt="coverImage"
-              />
-            )}
-          </div>
-          <div className="flex md:gap-14 gap-5">
-            <Avatar
-              url={channel?.avatar?.url}
-              fullName={channel?.fullName}
-              className="md:h-36 h-20 md:w-36 w-20"
-            />
-            {loading ? (
-              <div className="flex flex-col md:gap-4 gap-2">
-                <Skeleton className="max-w-[200px] w-64 h-6" />
-                <Skeleton className="max-w-[150px] w-44 h-4" />
-                <Skeleton className="max-w-[150px] w-44 h-4" />
+          {loading ? (
+            // Use ChannelLayoutSkeleton component while loading
+            <ChannelLayoutSkeleton />
+          ) : (
+            <>
+              {/* Channel cover image */}
+              <div className="w-full md:h-44 h-28 rounded-2xl overflow-hidden">
+                <img
+                  src={channel?.coverImage?.url || "/default-cover.png"}
+                  className="w-full h-full object-cover"
+                  alt="coverImage"
+                />
               </div>
-            ) : (
-              <div className="flex flex-col gap-1">
-                <h1
-                  className="md:text-4xl text-xl font-bold leading-none"
-                  title={channel?.fullName}
+              {/* Display channel details once loaded */}
+              <div className="flex md:gap-14 gap-5">
+                <Avatar
+                  url={channel?.avatar?.url}
+                  fullName={channel?.fullName}
+                  className="md:h-36 h-20 md:w-36 w-20"
+                />
+
+                <div className="flex flex-col gap-1">
+                  {/* Channel name */}
+                  <h1
+                    className="md:text-4xl text-xl font-bold"
+                    title={channel?.fullName}
+                  >
+                    {channel?.fullName}
+                  </h1>
+
+                  {/* Username and subscriber/video count */}
+                  <p className="md:text-lg text-sm text-zinc-600 font-semibold leading-none">
+                    {channel?.username}
+                  </p>
+                  <p className="md:text-lg text-sm text-zinc-600 font-semibold leading-none">
+                    {channel?.subscriberCount}{" "}
+                    {channel?.subscriberCount <= 1
+                      ? "Subscriber"
+                      : "Subscribers"}{" "}
+                    {" • "}
+                    {channel?.videoCount}{" "}
+                    {channel?.videoCount <= 1 ? "video" : "videos"}
+                  </p>
+                </div>
+              </div>
+            </>
+          )}
+
+          {/* Channel tabs */}
+          <div className="flex md:gap-10 gap-7 relative after:absolute after:-bottom-2 after:w-full after:h-[1px] after:bg-gray-300">
+            {channelTabsLink.map(({ label, slug }) => (
+              // Render channel tabs with appropriate styles
+              <Link
+                to={slug}
+                className={twMerge(
+                  "text-base text-zinc-500 font-semibold font-poppins transition-all",
+                  location.pathname === slug &&
+                    "text-black relative after:content-[''] after:absolute after:z-[2] after:-bottom-2 after:left-0 after:w-full after:h-[2px] after:bg-black"
+                )}
+              >
+                <button
+                  className="disabled:opacity-70 disabled:animate-pulse"
+                  disabled={loading}
                 >
-                  {channel?.fullName}
-                </h1>
-
-                <p className="md:text-lg text-sm text-zinc-800 font-medium leading-none">
-                  {channel?.username}
-                </p>
-                <p className="md:text-lg text-sm text-zinc-800 font-medium leading-none">
-                  {channel?.subscriberCount}{" "}
-                  {channel?.subscriberCount <= 1 ? "Subscriber" : "Subscribers"}
-                  {" • "}
-                  {channel?.videoCount}{" "}
-                  {channel?.videoCount <= 1 ? "video" : "videos"}
-                </p>
-              </div>
-            )}
-          </div>
-
-          {/* channel tabs */}
-          <div
-            className="flex gap-10 relative after:absolute after:-bottom-2 after:w-full
-         after:h-[1px] after:bg-gray-300"
-          >
-            <Link
-              to={`/c/${username}`}
-              className={twMerge(
-                "text-base text-gray-700 font-medium font-poppins transition-all",
-                location.pathname === `/c/${username}` &&
-                  "font-semibold relative after:content-[''] after:absolute after:z-[2] after:-bottom-2 after:left-0 after:w-full after:h-[1px] after:bg-black"
-              )}
-            >
-              Home
-            </Link>
-            <Link
-              to={`/c/${username}/videos`}
-              className={twMerge(
-                "text-base text-gray-700 font-medium font-poppins transition-all",
-                location.pathname.includes("/videos") &&
-                  "font-semibold relative after:content-[''] after:absolute after:z-[2] after:-bottom-2 after:left-0 after:w-full after:h-[1px] after:bg-black"
-              )}
-            >
-              Videos
-            </Link>
-            <Link
-              to={`/c/${username}/tweets`}
-              className={twMerge(
-                "text-base text-gray-700 font-medium font-poppins transition-all",
-                location.pathname.includes("/tweets") &&
-                  "font-semibold relative after:content-[''] after:absolute after:z-[2] after:-bottom-2 after:left-0 after:w-full after:h-[1px] after:bg-black"
-              )}
-            >
-              Tweets
-            </Link>
-            <Link
-              to={`/c/${username}/playlists`}
-              className={twMerge(
-                "text-base text-gray-700 font-medium font-poppins transition-all",
-                location.pathname.includes("/playlists") &&
-                  "font-semibold relative after:content-[''] after:absolute after:z-[2] after:-bottom-2 after:left-0 after:w-full after:h-[1px] after:bg-black"
-              )}
-            >
-              Playlists
-            </Link>
+                  {label}
+                </button>
+              </Link>
+            ))}
           </div>
         </div>
       )}
