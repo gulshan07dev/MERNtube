7350bd0d57b53472f00e0ed1e6f74c2e96415c81
gulshan07dev <gulshanku9135@gmail.com>
2024-01-22 13:29:11 +0530

    add them in CoreUi folder

--- client/src/component/CoreUI/Form.jsx
+++ client/src/component/CoreUI/Form.jsx
@@ -7,9 +7,11 @@ export default function Form({
   description,
   inputs,
   submitButtonLabel,
+  submitButtonIcon,
+  isSubmitButtonPositionLeft = true,
   onSubmit,
   isLoading,
-  error
+  error,
 }) {
   return (
     <form
@@ -17,7 +19,7 @@ export default function Form({
         e.preventDefault();
         onSubmit();
       }}
-      className="py-7 px-9 border flex flex-col gap-7"
+      className="py-7 md:px-9 px-4 border border-zinc-300 bg-slate-50 flex flex-col gap-7"
     >
       <div className="flex flex-col gap-[2px]">
         <h1 className="text-zinc-950 text-2xl font-roboto font-[600]">
@@ -26,7 +28,7 @@ export default function Form({
         <p className="text-zinc-700 text-lg font-poppins leading-5">
           {description}
         </p>
-      </div> 
+      </div>
       {error && (
         <div className="w-full flex gap-2 items-center border border-red-600 bg-red-50 shadow-sm rounded-sm p-2 ">
           <p className="text-red-600 text-base font-nunito w-full">{error}</p>
@@ -37,8 +39,11 @@ export default function Form({
       <Button
         label={isLoading ? "Loading..." : submitButtonLabel}
         type="submit"
+        icon={submitButtonIcon}
         disabled={isLoading}
-        className="w-fit px-8 text-base"
+        className={`w-fit px-8 text-base ${
+          isSubmitButtonPositionLeft ? "self-start" : "self-end"
+        }`}
       />
     </form>
   );
