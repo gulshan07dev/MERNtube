daa6ba830178b9f3b9f1ef89b2893d1356a6aae7
gulshan07dev <gulshanku9135@gmail.com>
2024-05-06 11:42:57 +0530

    segregate router with routes folder

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,11 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
-import AuthRequired from "./helper/auth/AuthRequired";
-import AuthNotRequired from "./helper/auth/AuthNotRequired";
 import { getCurrentUser, refreshAccessToken } from "./store/slices/authSlice";
 import { AppDispatch } from "./store/store";
 import { setAppLoading } from "./store/slices/appLoadingSlice";
-import Home from "./pages/Home";
-import Login from "./pages/auth/Login";
-import Signup from "./pages/auth/Signup";
-import Account from "./pages/Account";
-import ChannelLayout from "./layout/ChannelLayout";
-import Channel from "./pages/channel/Channel";
-import Videos from "./pages/channel/Videos";
-import Tweets from "./pages/channel/Tweets";
-import Playlists from "./pages/channel/Playlists";
-import Subscribers from "./pages/channel/Subscribers";
-import Create from "./pages/Create";
-import Settings from "./pages/Settings";
-import PlaylistVideos from "./pages/PlaylistVideos";
-import VideoPlayer from "./pages/VideoPlayer ";
-import WatchHistory from "./pages/WatchHistory";
-import WatchLater from "./pages/WatchLater";
-import LikedVideos from "./pages/LikedVideos";
-import SendFeedback from "./pages/SendFeedback";
-import Subscriptions from "./pages/Subscriptions";
-import Dashboard from "./pages/Dashboard";
+import router from "./routes";
 
 function App() {
   const dispatch: AppDispatch = useDispatch();
@@ -55,102 +34,6 @@ function App() {
       document.body.classList.add(theme);
     }
   }, []);
-
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "settings",
-      element: <Settings />,
-    },
-    {
-      path: "auth",
-      element: <AuthNotRequired />,
-      children: [
-        {
-          path: "login",
-          element: <Login />,
-        },
-        {
-          path: "signup",
-          element: <Signup />,
-        },
-      ],
-    },
-    {
-      element: <AuthRequired />,
-      children: [
-        {
-          path: "account",
-          element: <Account />,
-        },
-        {
-          path: "create",
-          element: <Create />,
-        },
-        {
-          path: "/c/:username",
-          element: <ChannelLayout />,
-          children: [
-            {
-              path: "",
-              element: <Channel />,
-            },
-            {
-              path: "videos",
-              element: <Videos />,
-            },
-            {
-              path: "tweets",
-              element: <Tweets />,
-            },
-            {
-              path: "playlists",
-              element: <Playlists />,
-            },
-            {
-              path: "subscribers",
-              element: <Subscribers />,
-            },
-          ],
-        },
-        {
-          path: "/dashboard",
-          element: <Dashboard />,
-        },
-        {
-          path: "send-feedback",
-          element: <SendFeedback />,
-        },
-        {
-          path: "feed/subscriptions",
-          element: <Subscriptions />,
-        },
-        {
-          path: "playlists/:playlistId",
-          element: <PlaylistVideos />,
-        },
-        {
-          path: "watch/:videoId",
-          element: <VideoPlayer />,
-        },
-        {
-          path: "feed/history",
-          element: <WatchHistory />,
-        },
-        {
-          path: "watch-later",
-          element: <WatchLater />,
-        },
-        {
-          path: "liked-videos",
-          element: <LikedVideos />,
-        },
-      ],
-    },
-  ]);
 
   return (
     <>
---
+++ client/src/routes/index.tsx
@@ -0,0 +1,145 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import RootLayout from "../layout/RootLayout";
+import AuthRequired from "../helper/auth/AuthRequired";
+import AuthNotRequired from "../helper/auth/AuthNotRequired";
+import Home from "../pages/Home";
+import Login from "../pages/auth/Login";
+import Signup from "../pages/auth/Signup";
+import Account from "../pages/Account";
+import ChannelLayout from "../layout/ChannelLayout";
+import Channel from "../pages/channel/Channel";
+import Videos from "../pages/channel/Videos";
+import Tweets from "../pages/channel/Tweets";
+import Playlists from "../pages/channel/Playlists";
+import Subscribers from "../pages/channel/Subscribers";
+import Create from "../pages/Create";
+import Settings from "../pages/Settings";
+import PlaylistVideos from "../pages/PlaylistVideos";
+import VideoPlayer from "../pages/VideoPlayer ";
+import WatchHistory from "../pages/WatchHistory";
+import WatchLater from "../pages/WatchLater";
+import LikedVideos from "../pages/LikedVideos";
+import SendFeedback from "../pages/SendFeedback";
+import Subscriptions from "../pages/Subscriptions";
+import Dashboard from "../pages/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    path: "auth",
+    element: <RootLayout showNavigation={false} />,
+    children: [
+      {
+        element: <AuthNotRequired />,
+        children: [
+          {
+            path: "login",
+            element: <Login />,
+          },
+          {
+            path: "signup",
+            element: <Signup />,
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        element: <AuthRequired />,
+        children: [
+          {
+            path: "account",
+            element: <Account />,
+          },
+          {
+            path: "create",
+            element: <Create />,
+          },
+          {
+            path: "/c/:username",
+            element: <ChannelLayout />,
+            children: [
+              {
+                path: "",
+                element: <Channel />,
+              },
+              {
+                path: "videos",
+                element: <Videos />,
+              },
+              {
+                path: "tweets",
+                element: <Tweets />,
+              },
+              {
+                path: "playlists",
+                element: <Playlists />,
+              },
+              {
+                path: "subscribers",
+                element: <Subscribers />,
+              },
+            ],
+          },
+          {
+            path: "feed/subscriptions",
+            element: <Subscriptions />,
+          },
+          {
+            path: "playlists/:playlistId",
+            element: <PlaylistVideos />,
+          },
+          {
+            path: "feed/history",
+            element: <WatchHistory />,
+          },
+          {
+            path: "watch-later",
+            element: <WatchLater />,
+          },
+          {
+            path: "liked-videos",
+            element: <LikedVideos />,
+          },
+        ],
+      },
+      {
+        path: "settings",
+        element: <Settings />,
+      },
+    ],
+  },
+
+  {
+    element: <RootLayout byDefaultSidebarHidden />,
+    children: [
+      {
+        element: <AuthRequired />,
+        children: [
+          {
+            path: "watch/:videoId",
+            element: <VideoPlayer />,
+          },
+          {
+            path: "/dashboard",
+            element: <Dashboard />,
+          },
+          {
+            path: "send-feedback",
+            element: <SendFeedback />,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+export default router;
