ff0d18ffe5ba8ac417e9b7cadf0bc710a5f9a026
gulshan07dev <gulshanku9135@gmail.com>
2024-05-17 17:02:44 +0530

    fix: Button

--- client/src/component/CoreUI/Button.tsx
+++ client/src/component/CoreUI/Button.tsx
@@ -24,7 +24,7 @@ export default function Button({
     <button
       className={twMerge(
         btnType == "simple-btn"
-          ? "rounded-[4px] flex gap-2 items-center justify-center border bg-[#ff1cf4] hover:opacity-60 transition-[background] text-white font-medium font-Noto_sans disabled:opacity-60"
+          ? "rounded-[4px] flex gap-2 items-center justify-center border-none bg-[#ff1cf4] hover:opacity-60 transition-[background] text-white font-medium font-Noto_sans disabled:opacity-60"
           : "size-10 flex justify-center items-center text-lg rounded-full text-black dark:text-white hover:bg-slate-100 focus-within:bg-slate-100 dark:hover:bg-[#171717] focus-within:dark:bg-[#171717]",
         btnType == "simple-btn" && isLarge
           ? ["px-4 py-3 text-sm"]
