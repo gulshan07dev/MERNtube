e50e00b5f7317cf2d08943d112f7938b3bc424c7
gulshan07dev <gulshanku9135@gmail.com>
2024-05-22 14:19:17 +0530

    fix: watch later slice

--- client/src/store/slices/watchLaterSlice.ts
+++ client/src/store/slices/watchLaterSlice.ts
@@ -1,112 +1,44 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"; 
-import axiosInstance from "@/helper/axiosInstance";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { IVideo } from "@/interfaces";
 
 interface initialState {
   watchLaterVideos: { _id: string; watchLaterVideo: IVideo }[];
-  loading: boolean;
-  error: string | null;
-  currentPage: number;
-  totalPages: number;
-  totalVideos: number;
-  hasNextPage: boolean;
+  paginationInfo: {
+    currentPage: number;
+    totalPages: number;
+    totalDocs: number;
+    hasNextPage: boolean;
+  };
 }
 
-const initialState:initialState = {
+const initialState: initialState = {
   watchLaterVideos: [],
-  loading: false,
-  error: null,
-  currentPage: 0,
-  totalPages: 0,
-  totalVideos: 0,
-  hasNextPage: false,
+  paginationInfo: {
+    currentPage: 0,
+    totalPages: 0,
+    totalDocs: 0,
+    hasNextPage: false,
+  },
 };
-
-const addVideoToWatchLater = createAsyncThunk(
-  "/watch_later/add/videoId",
-  async (videoId: string, { rejectWithValue }) => {
-    try {
-      const res = await axiosInstance.post(`/watch_later/${videoId}`);
-      return res?.data;
-    } catch (error: any) {
-      if (!error.response) {
-        throw error;
-      }
-      return rejectWithValue(error?.response?.data);
-    }
-  }
-);
-
-const removeVideoFromWatchLater = createAsyncThunk(
-  "/watch_later/remove/videoId",
-  async (videoId: string, { rejectWithValue }) => {
-    try {
-      const res = await axiosInstance.delete(`/watch_later/${videoId}`);
-      return res?.data;
-    } catch (error: any) {
-      if (!error.response) {
-        throw error;
-      }
-      return rejectWithValue(error?.response?.data);
-    }
-  }
-);
-
-const getUserWatchLaterVideos = createAsyncThunk(
-  "/watch_later/get",
-  async (
-    { queryParams }: { queryParams: { page?: number; limit?: number } },
-    { rejectWithValue }
-  ) => {
-    try {
-      const res = await axiosInstance.get("/watch_later", {
-        params: queryParams,
-      });
-      return res?.data;
-    } catch (error: any) {
-      if (!error.response) {
-        throw error;
-      }
-      return rejectWithValue(error?.response?.data);
-    }
-  }
-);
 
 const watchLaterSlice = createSlice({
   name: "watch_later",
   initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(getUserWatchLaterVideos.pending, (state) => {
-        state.loading = true;
-        state.error = null;
-        state.hasNextPage = false;
-      })
-      .addCase(getUserWatchLaterVideos.fulfilled, (state, action) => {
-        const new_watchLaterVideos = action.payload.data?.result?.docs;
-        state.watchLaterVideos =
-          action.payload.data?.result?.page == 1
-            ? new_watchLaterVideos
-            : [...state.watchLaterVideos, ...new_watchLaterVideos];
-        state.loading = false;
-        state.error = null;
-        state.currentPage = action.payload.data?.result?.page;
-        state.totalVideos = action.payload.data?.result?.totalDocs || 0;
-        state.totalPages = action.payload.data?.result?.totalPages;
-        state.hasNextPage = action.payload.data?.result?.hasNextPage;
-      })
-      .addCase(getUserWatchLaterVideos.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.error.message || "Failed to fetch watch later videos";
-      });
+  reducers: {
+    setWatchLaterVideos: (
+      state,
+      action: PayloadAction<initialState["watchLaterVideos"]>
+    ) => {
+      state.watchLaterVideos = action.payload;
+    },
+    setPaginationInfo: (
+      state,
+      action: PayloadAction<initialState["paginationInfo"]>
+    ) => {
+      state.paginationInfo = action.payload;
+    },
   },
 });
 
 export default watchLaterSlice.reducer;
-export const {} = watchLaterSlice.actions;
-export {
-  addVideoToWatchLater,
-  removeVideoFromWatchLater,
-  getUserWatchLaterVideos,
-};
+export const { setWatchLaterVideos, setPaginationInfo } = watchLaterSlice.actions;
